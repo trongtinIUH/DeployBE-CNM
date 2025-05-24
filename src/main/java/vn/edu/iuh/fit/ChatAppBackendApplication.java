98569a95863f5ed7d0e4dea2999319303565b5bf
trongtinIUH <trantin1973@gmail.com>
2025-05-24 15:47:47 +0700

    bootjar be

--- src/main/java/vn/edu/iuh/fit/ChatAppBackendApplication.java
+++ src/main/java/vn/edu/iuh/fit/ChatAppBackendApplication.java
@@ -7,7 +7,7 @@ import vn.edu.iuh.fit.config.AwsConfig;
 @EnableCaching
 @SpringBootApplication
 public class ChatAppBackendApplication {
-
+//deploy BE
     public static void main(String[] args) {
         SpringApplication.run(ChatAppBackendApplication.class, args);
     }
