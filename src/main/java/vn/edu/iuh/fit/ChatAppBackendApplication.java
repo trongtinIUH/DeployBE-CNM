3b4220e634cf68286a37e46ae83abed9e0d51e75
trongtinIUH <trantin1973@gmail.com>
2025-05-24 16:22:32 +0700

    ok la

--- src/main/java/vn/edu/iuh/fit/ChatAppBackendApplication.java
+++ src/main/java/vn/edu/iuh/fit/ChatAppBackendApplication.java
@@ -7,7 +7,7 @@ import vn.edu.iuh.fit.config.AwsConfig;
 @EnableCaching
 @SpringBootApplication
 public class ChatAppBackendApplication {
-//deploy BEffew
+//deploy BEffewfewf
     public static void main(String[] args) {
         SpringApplication.run(ChatAppBackendApplication.class, args);
     }
