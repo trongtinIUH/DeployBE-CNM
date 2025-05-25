91209474a35a9472c907d161ac8ed20ba8463361
trongtinIUH <trantin1973@gmail.com>
2025-05-25 15:44:15 +0700

    oke la11

--- src/main/java/vn/edu/iuh/fit/config/WebConfig.java
+++ src/main/java/vn/edu/iuh/fit/config/WebConfig.java
@@ -1,27 +1,24 @@
 package vn.edu.iuh.fit.config;
 
-
-import lombok.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        String socketOrigin = System.getenv("web.socket");
-        return new WebMvcConfigurer() {
+        String socketOrigin = System.getenv("web.socket"); // lấy từ biến môi trường Railway
 
+        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 socketOrigin,
                                 "http://localhost:3000",
-                                "http://192.168.100.18:3000",
-                                "https://0c94-109-237-64-222.ngrok-free.app" // FE chạy qua ngrok
+                                "https://your-fe.localtunnel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
@@ -30,3 +27,4 @@ public class WebConfig {
         };
     }
 }
+
