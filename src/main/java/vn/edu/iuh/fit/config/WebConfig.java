731a0d85225b142ac7f337d48f26d1fb86f4c623
trongtinIUH <trantin1973@gmail.com>
2025-05-25 10:05:20 +0700

    bootjar1112323

--- src/main/java/vn/edu/iuh/fit/config/WebConfig.java
+++ src/main/java/vn/edu/iuh/fit/config/WebConfig.java
@@ -1,6 +1,6 @@
 package vn.edu.iuh.fit.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,18 +8,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-    private final Dotenv dotenv = Dotenv.load();
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Áp dụng cho tất cả endpoint
-                        .allowedOrigins("http://localhost:3000",dotenv.get("web.socket"),"http://192.168.100.18:3000","https://1d8a-109-237-64-222.ngrok-free.app") // Chỉ định domain frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các method được phép
-                        .allowedHeaders("*") // Cho phép tất cả header
-                        .allowCredentials(true); // Cần bật để gửi cookie/session qua domain khác
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://192.168.100.18:3000",
+                                "https://1d8a-109-237-64-222.ngrok-free.app" // FE chạy qua ngrok
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
